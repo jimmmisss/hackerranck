508eda1cf07011220530389caf5e93950f47f7f4
Wesley Pereira <jimmmisss@gmail.com>
2023-08-21 13:14:15 -0300

    Int palindrome

--- src/main/java/com/hackerrank/StringIntegerReversePalindrome.java
+++ src/main/java/com/hackerrank/StringIntegerReversePalindrome.java
@@ -6,6 +6,12 @@ public class StringIntegerReversePalindrome {
 
     public static void main(String[] args) {
 
+        //stringNumberPalindrome();
+        integerPalindrome();
+
+    }
+
+    private static void stringNumberPalindrome() {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         int strLength = str.length();
@@ -21,6 +27,25 @@ public class StringIntegerReversePalindrome {
         } else {
             System.out.println(str + " is not a palindrome string.");
         }
+    }
 
+    private static void integerPalindrome() {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int reverseNum = 0, remainder;
+
+        int originalNum = num;
+
+        while (num != 0) {
+            remainder = num % 10;
+            reverseNum = reverseNum * 10 + remainder;
+            num /= 10;
+        }
+        if (originalNum == reverseNum) {
+            System.out.println(originalNum + " is palindrome");
+        } else {
+            System.out.println(originalNum + " not is palindrome");
+        }
     }
 }
